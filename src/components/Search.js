440a1f300b169d3a73085c48a9fd126d5eff206b
dell_di <875372314@qq.com>
2019-12-27 17:31:16 +0800

    修改配置和全剧响应

---
+++ src/components/Search.js
@@ -0,0 +1,25 @@
+import React, {useState } from 'react';
+
+const Search = (props) => {
+  const [searchValue, setSearchValue] = useState(""); // 定义状态searchValue，设置状态
+  const handleSearchInputChange = e => {
+    setSearchValue(e.target.value)
+  }
+  const resetInputField = () => {
+    setSearchValue('')
+  }
+  const callSearchFunction = e => {
+    e.preventDefault()
+    props.search(searchValue)
+    resetInputField()
+  }
+
+  return (
+    <form className="search">
+      <input type="text" onChange={handleSearchInputChange} value={searchValue} />
+      <input type="submit" onClick={callSearchFunction} value="Search" />
+    </form>
+  )
+}
+
+export default Search
